b74389f9b257c3a00c3483d342644079571f5b67
User <francisnijr.pro@gmail.com>
2025-04-17 20:50:45 -0300

    Adding contact page

---
+++ src/lib/components/stable/Contact.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { Textarea } from 'flowbite-svelte';
+    import { AnnotationSolid, ChevronRightOutline, MailBoxSolid } from 'flowbite-svelte-icons';
+
+    let value = $state('');
+   let mailto = "mailto:whoami.spacemail@gmail.com?subject="
+   
+  </script>
+
+  
+  
+  <Textarea 
+  class=" bg-1 text-2 p-minimal"
+  id="textarea-id" placeholder="Write your message here or click the button below to go directly..." rows="4" name="message" bind:value defaultValue="" />
+ <div class="w-full flex justify-center items-center">
+    <button class="mt-minimal bg-2 text-3 font-bold rounded-xl mx-auto text-minimal cursor-pointer"><a href={mailto + value } class="redirect p-minimal flex  justify-center items-center  gap-2">Send by email<AnnotationSolid class="w-minimal h-minimal" /></a></button>
+ </div>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Main from "$lib/components/custom/Main.svelte";
+    import Textarea from "$lib/components/stable/Contact.svelte";
+</script>
+
+
+
+<Main custom="flex flex-col gap-medium" >
+    <div class="flex flex-col gap-medium">
+        <h1 class="text-maximal text-center" >Help? Doubts? Colaborations?</h1>
+    <h2 class="text-medium text-center">Contact me by email and I will respond as quickly as possible:</h2>
+    </div>
+    <div class="">
+        <Textarea />
+    </div>
+</Main>
